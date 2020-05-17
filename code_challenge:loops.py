71f7411609a220d151bae2f2995909ec28ab4888
Darryl <darryl@zerolinux.localdomain>
2020-05-16 21:01:39 -0600

    Completed CODE CHALLENGE: LOOPS on CodeAcademy.

--- code_challenge:loops.py
+++ code_challenge:loops.py
@@ -87,3 +87,119 @@ For example, consider the following code.
 exponents([2, 3, 4], [1, 2, 3])
 the result would be the list [2, 4, 8, 3, 9, 27, 4, 16, 64]. It would first add two to the first. Then two to the second. Then two to the third, and so on.
 """
+#Write your function here
+def exponents(bases,powers):
+  new_lst = []
+  for base in bases:
+    for power in powers:
+      new_lst.append(base ** power)
+  return new_lst
+#Uncomment the line below when your function is done
+print(exponents([2, 3, 4], [1, 2, 3]))
+
+"""
+Larger Sum
+
+Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+
+The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+"""
+#Write your function here
+def larger_sum(lst1,lst2):
+  sum_of_lst1 = 0
+  sum_of_lst2 = 0
+  for x in lst1:
+    sum_of_lst1 += x
+  for x in lst2:
+    sum_of_lst2 += x
+  if sum_of_lst1 == sum_of_lst2 or sum_of_lst1 > sum_of_lst2:
+    return lst1
+  else:
+    return lst2
+#Uncomment the line below when your function is done
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+"""
+Over 9000
+
+Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+
+The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+
+For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+"""
+#Write your function here
+def over_nine_thousand(lst):
+  if len(lst) == 0:
+    return 0
+  sum_of_lst = 0
+  for x in lst:
+    if sum_of_lst < 9000:
+      sum_of_lst += x
+    elif sum_of_lst > 9000:
+      return sum_of_lst
+  return sum_of_lst
+#Uncomment the line below when your function is done
+print(over_nine_thousand([8000, 900, 120, 5000]))
+
+"""
+Max Num
+
+Create a function named max_num() that takes a list of numbers named nums as a parameter.
+
+The function should return the largest number in nums
+"""
+#Write your function here
+def max_num(nums):
+  x = nums[0]
+  for num in nums:
+    if num > x:
+      x = num
+  return x
+#Uncomment the line below when your function is done
+print(max_num([50, -10, 0, 75, 20]))
+
+"""
+Same Values
+
+Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+
+The function should return a list of the indices where the values were equal in lst1 and lst2.
+
+For example, the following code should return [0, 2, 3]
+
+same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+"""
+#Write your function here
+def same_values(lst1,lst2):
+  new_lst = []
+  for x in range(len(lst1)):
+    if lst1[x] == lst2[x]:
+      new_lst.append(x)
+  return new_lst
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+"""
+Reversed List
+
+Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+
+The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+
+For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+"""
+#Write your function here
+def reversed_list(lst1,lst2):
+  rev_lst1 = []
+  rev_index = -1
+  for x in lst1:
+    rev_lst1.append(lst1[rev_index])
+    rev_index += -1
+  if rev_lst1 == lst2:
+    return True
+  else:
+    return False
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
